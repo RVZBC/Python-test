dc866e7e21ef1a98bde7ae3ac2969d1195c38163
Rowell Verdejo <rowellverdejo@gmail.com>
2023-01-16 20:25:36 +0800

    Initial commit

---
+++ App.py
@@ -0,0 +1,22 @@
+course = 'Python for Beginners'
+print('Python' in course)
+
+print("Hello world")
+
+age = 20
+print(age)
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+
+age = 2023 - int(birth_year)
+
+print(age)
+
+
+first = int(input("First: "))
+second = int(input("Second: "))
+sum = first + second
+print("Sum: " + str(sum))
